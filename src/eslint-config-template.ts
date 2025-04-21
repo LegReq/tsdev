16462a9937412d574d95a659984e73dec9524ef6
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-04-21 18:41:52 -0400

    Updated ESLint to align with company requirements.

--- src/eslint-config-template.ts
+++ src/eslint-config-template.ts
@@ -17,24 +17,27 @@ import js from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import esLintConfigLove from "eslint-config-love";
 import jsdoc from "eslint-plugin-jsdoc";
-// @ts-expect-error -- No .d.ts file available, pending https://github.com/nickdeis/eslint-plugin-notice/issues/32.
 import notice from "eslint-plugin-notice";
 import tseslint, { type ConfigArray } from "typescript-eslint";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export const esLintConfigLegReq: ConfigArray = tseslint.config(
     {
-        ignores: ["eslint.config.ts", "dist"]
+        ignores: ["dist"]
     },
     js.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
-    stylistic.configs["recommended-flat"],
+    stylistic.configs.recommended,
     jsdoc.configs["flat/recommended-typescript"],
     esLintConfigLove,
     {
         languageOptions: {
             parserOptions: {
                 projectService: true
-            }
+            },
+            globals: globals.browser
         },
 
         linterOptions: {
@@ -42,16 +45,19 @@ export const esLintConfigLegReq: ConfigArray = tseslint.config(
         },
 
         plugins: {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- No .d.ts file available, pending https://github.com/nickdeis/eslint-plugin-notice/issues/32.
+            reactHooks,
+            reactRefresh,
             notice
         },
 
         rules: {
+            ...reactHooks.configs.recommended.rules,
+
             "complexity": "off",
             "max-depth": ["error", 10],
             "max-lines": "off",
-            "no-dupe-class-members": "off",
-            "no-redeclare": "off",
+
+            // Handled by @typescript-eslint/no-unused-vars.
             "no-unused-vars": "off",
 
             "@typescript-eslint/class-literal-property-style": "off",
@@ -60,7 +66,13 @@ export const esLintConfigLegReq: ConfigArray = tseslint.config(
             "@typescript-eslint/max-params": "off",
             "@typescript-eslint/no-empty-function": "off",
             "@typescript-eslint/no-empty-object-type": "off",
-            "@typescript-eslint/no-magic-numbers": "off",
+            "@typescript-eslint/no-magic-numbers": [
+                "error",
+                {
+                    ignore: [0, 1, -1, 2, 4],
+                    ignoreReadonlyClassProperties: true
+                }
+            ],
             "@typescript-eslint/no-unnecessary-type-parameters": "off",
             "@typescript-eslint/no-unused-vars": [
                 "error",
@@ -71,58 +83,116 @@ export const esLintConfigLegReq: ConfigArray = tseslint.config(
                 }
             ],
             "@typescript-eslint/prefer-destructuring": "off",
-            "@typescript-eslint/unbound-method": ["error", {
-                ignoreStatic: true
-            }],
+            "@typescript-eslint/unbound-method": [
+                "error",
+                {
+                    ignoreStatic: true
+                }
+            ],
+
+            "react-refresh/only-export-components": [
+                "warn",
+                {
+                    allowConstantExport: true
+                }
+            ],
 
             "@stylistic/array-bracket-newline": ["error", "consistent"],
-            "@stylistic/brace-style": ["error", "1tbs", {
-                allowSingleLine: false
-            }],
+            "@stylistic/brace-style": [
+                "error",
+                "1tbs",
+                {
+                    allowSingleLine: false
+                }
+            ],
             "@stylistic/comma-dangle": ["error", "never"],
             "@stylistic/indent": ["error", 4],
-            "@stylistic/member-delimiter-style": ["error", {
-                multiline: {
-                    delimiter: "semi",
-                    requireLast: true
-                },
-                singleline: {
-                    delimiter: "semi"
+            "@stylistic/jsx-closing-bracket-location": "off",
+            "@stylistic/jsx-closing-tag-location": "off",
+            "@stylistic/jsx-curly-spacing": [
+                "error",
+                {
+                    when: "never",
+                    children: true
                 }
-            }],
+            ],
+            "@stylistic/jsx-indent-props": ["error", 4],
+            "@stylistic/jsx-wrap-multilines": "off",
+            "@stylistic/member-delimiter-style": [
+                "error",
+                {
+                    multiline: {
+                        delimiter: "semi",
+                        requireLast: true
+                    },
+                    singleline: {
+                        delimiter: "semi"
+                    }
+                }
+            ],
             "@stylistic/no-trailing-spaces": ["off"],
             "@stylistic/operator-linebreak": ["error", "after"],
             "@stylistic/quotes": ["error", "double"],
             "@stylistic/semi": ["error", "always"],
-            "@stylistic/object-curly-newline": ["error", {
-                ObjectExpression: {
-                    multiline: true,
-                    minProperties: 1
-                },
-                ObjectPattern: {
-                    multiline: true,
-                    minProperties: 1
+            "@stylistic/object-curly-newline": [
+                "error",
+                {
+                    ObjectExpression: {
+                        multiline: true,
+                        minProperties: 1
+                    },
+                    ObjectPattern: {
+                        multiline: true,
+                        minProperties: 1
+                    }
                 }
-            }],
+            ],
             "@stylistic/object-property-newline": "error",
 
-            "jsdoc/require-description": ["warn", {
-                contexts: ["ClassDeclaration", "ClassProperty", "FunctionDeclaration", "MethodDefinition", "TSEnumDeclaration", "TSInterfaceDeclaration", "TSModuleDeclaration", "TSTypeAliasDeclaration"]
-            }],
-            "jsdoc/require-jsdoc": ["warn", {
-                contexts: ["ClassDeclaration", "ClassProperty", "FunctionDeclaration", "MethodDefinition", "TSEnumDeclaration", "TSInterfaceDeclaration", "TSModuleDeclaration", "TSTypeAliasDeclaration"]
-            }],
-            "jsdoc/require-returns": ["warn", {
-                checkGetters: false
-            }],
-            "jsdoc/tag-lines": ["warn", "any", {
-                count: 1,
-                startLines: 1
-            }],
-
-            "notice/notice": ["error", {
-                templateFile: "node_modules/@legreq/tsdev/src/copyright.ts"
-            }]
+            "jsdoc/require-description": [
+                "warn",
+                {
+                    contexts: [
+                        "ClassDeclaration",
+                        "ClassProperty",
+                        "FunctionDeclaration",
+                        "MethodDefinition",
+                        "TSEnumDeclaration",
+                        "TSInterfaceDeclaration",
+                        "TSModuleDeclaration",
+                        "TSTypeAliasDeclaration"
+                    ]
+                }
+            ],
+            "jsdoc/require-jsdoc": [
+                "warn",
+                {
+                    contexts: [
+                        "ClassDeclaration",
+                        "ClassProperty",
+                        "FunctionDeclaration",
+                        "MethodDefinition",
+                        "TSEnumDeclaration",
+                        "TSInterfaceDeclaration",
+                        "TSModuleDeclaration",
+                        "TSTypeAliasDeclaration"
+                    ]
+                }
+            ],
+            "jsdoc/require-returns": [
+                "warn",
+                {
+                    checkGetters: false
+                }
+            ],
+            "jsdoc/tag-lines": [
+                "warn",
+                "any",
+                {
+                    count: 1,
+                    startLines: 1
+                }
+            ]
         }
     },
     {
@@ -133,6 +203,7 @@ export const esLintConfigLegReq: ConfigArray = tseslint.config(
             "max-nested-callbacks": "off",
             "jsdoc/require-jsdoc": "off",
             "@typescript-eslint/dot-notation": "off",
+            "@typescript-eslint/no-magic-numbers": "off",
             "@typescript-eslint/no-unsafe-type-assertion": "off"
         }
     }
